4dd4e3294befc92327fa2c0be474b46ed9b2bb60
Daniil Kostrykin <daniilkostrykin1@gmail.com>
2024-07-25 17:37:14 +0300

    allwithoutfinisheddesign

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,71 @@
+const hangmanImage = document.querySelector(".hangman-box img");
 const keyboardDiv = document.querySelector(".keyboard");
 const guessesText = document.querySelector(".guesses-text b");
 const wordDisplay = document.querySelector(".word-display");
-
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = document.querySelector(".play-again");
 let currentWord,
-  wrongGuessCount = 0;
+  correctLetters = [];
+wrongGuessCount = 0;
 const maxGuesses = 6;
+
+const resetGame = () => {
+  correctLetters = [];
+  wrongGuessCount = 0;
+  wordDisplay.innerHTML = currentWord
+    .split("")
+    .map(() => '<li class="letter"></li>')
+    .join("");
+  gameModal.classList.remove("show");
+  hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+};
 const getRandomWord = () => {
   const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
   currentWord = word;
   console.log(word);
   document.querySelector(".hint-text b").innerText = hint;
-  wordDisplay.innerHTML = word
-    .split("")
-    .map(() => '<li class="letter"></li>')
-    .join("");
+  resetGame();
+};
+const gameOver = (isVictory) => {
+  setTimeout(() => {
+    const modalText = isVictory
+      ? `You found the word: `
+      : `The correct word was: `;
+    gameModal.querySelector("img").src = `images/${
+      isVictory ? `victory` : `lost`
+    }.gif`;
+    gameModal.querySelector("h4").innerText = `${
+      isVictory ? `Congratulations!` : `Game Over!`
+    }`;
+    gameModal.querySelector(
+      "p"
+    ).innerHTML = `${modalText} <b> ${currentWord}</b>`;
+    gameModal.classList.add("show");
+  }, 300);
 };
 
 const initGame = (button, clickedLetter) => {
-  if (currentWord.includes(clickedLetter))
+  if (currentWord.includes(clickedLetter)) {
     //Showing correct letters
     [...currentWord].forEach((letter, index) => {
-      if (letter === clickedLetter)
+      if (letter === clickedLetter) {
+        correctLetters.push(letter);
         wordDisplay.querySelectorAll("li")[index].innerText = letter;
-      if (letter === clickedLetter)
         wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
     });
-  else {
+  } else {
     wrongGuessCount++;
+    hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
   }
+
+  button.disabled = true;
   guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+  if (wrongGuessCount === maxGuesses) return gameOver(false);
+  if (correctLetters.length === currentWord.length) return gameOver(true);
 };
 
 for (let i = 1040; i < 1072; i++) {
@@ -40,3 +77,4 @@ for (let i = 1040; i < 1072; i++) {
   );
 }
 getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
